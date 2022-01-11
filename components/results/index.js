ef794618f38f1a2e986a7d8820e27cc6f110d2a5
hadihaider055 <haiderhadi055@gmail.com>
2022-01-11 19:34:25 +0500

    Changes in .eslintrc.json

--- components/results/index.js
+++ components/results/index.js
@@ -3,8 +3,8 @@ import FlipMove from "react-flip-move";
 
 const Results = ({ results }) => {
   return (
-    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
-      {results.map((result) => (
+    <FlipMove className="flex-wrap justify-center px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex">
+      {results?.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
     </FlipMove>
